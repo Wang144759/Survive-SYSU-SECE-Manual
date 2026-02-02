0dd1c39d17f3fe3c0a12bd4141274bdc67191a85
LiuZongqi <1323746368@qq.com>
2026-02-02 23:42:08 +0800

    modified:   src/.vuepress/config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,11 +8,12 @@ export default defineUserConfig({
   title: "中山大学本科生自救指南(电通版)",
   description: "中山大学电子与通信工程学院（SECE）课程攻略、生存指南与资源汇总",
 
-  head: [
-    // 设置浏览器标签页的小图标 (Favicon)
-    ["link", { rel: "icon", href: "/logo.png" }],
-    ["meta", { name: "keywords", content: "中山大学, SECE, 电通院, 课程攻略, 选课指南, 考研" }],
-  ],
+head: [
+  // 标准 favicon（搜索引擎 & 地址栏）
+  ["link", { rel: "icon", href: "/favicon.ico" }],
+  ["meta", { name: "keywords", content: "中山大学, SECE, 电通院, 课程攻略, 选课指南, 考研" }],
+],
+
 
   bundler: viteBundler(),
   
